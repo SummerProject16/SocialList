2c1b141b44d51c8406543978a0d2992b7b810279
Madhu Kumar Dadi <mkdadi@outlook.in>
2016-05-07 22:18:24 +0530

    Change to testFile

--- CMUTweetTagger/testFile.py
+++ CMUTweetTagger/testFile.py
@@ -7,7 +7,7 @@ i=0
 for data in datasets:
     print i
     data1=data.split(" ")
-    if i<10000 and (data1[1] == '1' or data1[1] == '2'):
+    if i<9999 and (data1[1] == '1' or data1[1] == '2'):
         segData.append(" ".join(ws.segment(data1[0])))
     i+=1
 print "done"
