26b9d6eb6a98d652caba01fb8492ac68dd91fb9c
Madhu Kumar Dadi <mkdadi@outlook.in>
2016-05-11 16:03:45 +0530

    Updated

--- CMUTweetTagger/searchWeb.py
+++ CMUTweetTagger/searchWeb.py
@@ -17,13 +17,16 @@ def searchgoogle(text):
         retdata.append(url[0])
     return retdata
 
-def searchforstring(url,string):
+def searchforstring(url,stringlist):
     proxy = ulib.ProxyHandler({'http': "http://10.3.100.207:8080",'https': "https://10.3.100.207:8080"})
     opener = ulib.build_opener(proxy)
     ulib.install_opener(opener)
     req = ulib.Request(url, headers={'User-Agent' : "Mozilla/5.0"})
     dumpdata=ulib.urlopen(req)
-    if string in dumpdata.read():
-        return True
-    else:
-        return False
+    dump = dumpdata.read()
+    for string in stringlist:
+        if string in dump:
+            continue
+        else:
+            return False
+    return True
--- CMUTweetTagger/testFile14.py
+++ CMUTweetTagger/testFile14.py
@@ -1,5 +1,6 @@
 import wordsegment as ws
 import searchWeb
+import CMUTweetTagger as cmu
 
 file = open('../label_idiom.txt')
 
@@ -17,20 +18,35 @@ parsedSociallists = []
 for line in sociallists:
     parsedSociallists.append(" ".join(ws.segment(line)))
 
+postags = cmu.runtagger_parse(parsedSociallists)
+j = 0
 for line in parsedSociallists:
+    nounpart = []
+    k = 0
+    splitline = line.split()
+    for x in postags[j]:
+        if (x is 'M' or x is '^' or x is 'Z'):
+            nounpart.append(splitline[k])
+        k += 1
     googledata = searchWeb.searchgoogle(line)
     count = 0
+    print "Noun "+" ".join(nounpart)
+    if " ".join(nounpart) == "":
+        j+=1
+        print "Not required for "+line
+        continue
     i = 1
     for site in googledata:
         try:
-            if searchWeb.searchforstring(site,line):
-                count+=1
+            if searchWeb.searchforstring(site,nounpart):
+                count += 1
         except:
-            print "Network Failed for "+site
+            print "Network Failed for " + site
         i += 1
-        if i>10:
+        if i > 10:
             break
     if count > 5:
         print line + " True"
     else:
-        print line+" False"
+        print line + " False"
+    j += 1
