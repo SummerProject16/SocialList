d5a1112ea2a5d9b8a0253f97b484b880f6e6bf5e
root <root@localhost.localdomain>
2016-05-16 23:10:24 +0530

    added postags

---
+++ CMUTweetTagger/postag_s.py
@@ -0,0 +1,10 @@
+from wordsegment import segment
+from nltk import pos_tag, word_tokenize
+file = open('../label_idiom.txt')
+idiomsEx = file.readlines()
+for line in idiomsEx:
+    line = line.split()
+    if(line[1]=="1"):
+        a = segment(line[0])
+        a = pos_tag(a)
+        print a
