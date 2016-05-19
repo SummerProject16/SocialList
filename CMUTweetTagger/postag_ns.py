923c95144eb1b0dafbf9b1959776f14d16df74ae
Madhu Sai <madhusairavada@gmail.com>
2016-05-20 03:49:59 +0530

    stud commit

--- CMUTweetTagger/postag_ns.py
+++ CMUTweetTagger/postag_ns.py
@@ -2,9 +2,14 @@ from wordsegment import segment
 from nltk import pos_tag, word_tokenize
 file = open('../label_idiom.txt')
 idiomsEx = file.readlines()
+file1 = open('../postags1.csv','w')
 for line in idiomsEx:
     line = line.split()
-    if(line[1]=="0"):
+    if(line[1]=="1"):
         a = segment(line[0])
+        file1.write(line[0]+",")
         a = pos_tag(a)
-        print a
+        x = []
+        for i in range(len(a)):
+            x.append(a[i][1])
+        file1.write(",".join(x)+"\n")
