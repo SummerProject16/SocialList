65d0adc55f2cfe7e210055eb09b0e3cad8ce288a
madhusai3113 <madhusairavada@gmail.com>
2016-05-23 23:40:18 +0530

    added #comments

--- CMUTweetTagger/testFile4.py
+++ CMUTweetTagger/testFile4.py
@@ -1,24 +1,24 @@
 import wordsegment as ws
 import searchWeb
 import re
-
+#Presence of days in the line 
 file = open('../label_idiom.txt')
 
 idiomsEx = file.readlines()
 
 sociallists = []
 k=0
+#taking only the string
 for lines in idiomsEx:
     idiomset = lines.split()
     if idiomset[1] == '0':
         sociallists.append(idiomset[0])
-print "Sentences which contain numbers"
-print "---------------------"
+
 arr = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
 for line in sociallists:
     if any(x in line.lower() for x in arr):
         print line
         k=k+1
-print "---------"
+#number of Sentences which contain numbers
 print k
 
