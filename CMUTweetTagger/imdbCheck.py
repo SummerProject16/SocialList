f5d5f71f1368d689208b8d6f9268806ca3dc0ca3
Madhu Kumar Dadi <mkdadi@outlook.in>
2016-05-24 18:06:30 +0530

    Modified

--- CMUTweetTagger/imdbCheck.py
+++ CMUTweetTagger/imdbCheck.py
@@ -2,7 +2,6 @@ def checkStringinMovies(string):
     file = open("../imdbMovies.txt")
     data = file.readlines()
     for x in data:
-        if x.lower().replace("\n","") in string.lower():
-            print x
+        if x.lower().replace("\n","") == string.lower():
             return True
     return False
--- a.py
+++ a.py
@@ -1,18 +1,49 @@
-import re
-import urllib2 as ulib
+file1 = open("final_idiom_3_5_16.txt")
+file2 = open("final_idiom_3_5_161.txt")
+file3 = open("label10k_3_5_16.txt")
+file4 = open("label_idiom.txt")
+file5 = open("final_eliminated.txt")
 
-fileto = open("TopimdbMovies.txt", "w")
+file = open("socialLists.txt","w")
 
-proxy = ulib.ProxyHandler({'https': "https://10.3.100.207:8080", 'http': "http://10.3.100.207:8080"})
-opener = ulib.build_opener(proxy)
-ulib.install_opener(opener)
-req = ulib.Request('http://www.imdb.com/search/title?title_type=tv_series', headers={'User-Agent': "Mozilla/5.0"})
-dumpdata = ulib.urlopen(req)
+data1 = file1.readlines()
+data2 = file2.readlines()
+data3 = file3.readlines()
+data4 = file4.readlines()
+data5 = file5.readlines()
 
-a = dumpdata.read()
+data = []
 
-data = re.findall('<td.*?title.*?>.*?<a.*?>(.*?)</a>.*?</td>', a,re.DOTALL)
+for x in data1:
+    s = x.split()
+    if s[1] == '1':
+        data.append(s[0])
+
+for x in data2:
+    s = x.split()
+    if s[1] == '1':
+        data.append(s[0])
+
+for x in data3:
+    s = x.split()
+    if s[1] == '1':
+        data.append(s[0])
+
+for x in data4:
+    s = x.split()
+    if s[1] == '1':
+        data.append(s[0])
+
+for x in data5:
+    s = x.split()
+    if s[1] == '1':
+        data.append(s[0])
+        
+final = []
 
 for x in data:
-    fileto.write(x + "\n")
-    print x
+    if x not in final:
+        final.append(x)
+        
+for x in final:
+    file.write(x+"\n")
